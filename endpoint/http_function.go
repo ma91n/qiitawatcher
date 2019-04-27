eb71d7be9e118b490045a4fb39d6bc6b115b34ef
laqiiz <mano.junki@gmail.com>
2019-04-27 18:54:13 +0900

    call for cloud function

---
+++ endpoint/http_function.go
@@ -0,0 +1,26 @@
+package endpoint
+
+import (
+	"net/http"
+	"log"
+	"time"
+	"os"
+	"qiitawatcher/controller"
+)
+
+// http endpoint for Google Cloud Function
+func Receive(w http.ResponseWriter, r *http.Request) {
+	log.Printf("request received")
+
+	// override created time
+	beforeDate := time.Now().Add(-24 * time.Hour).Format("2006-01-02")
+	os.Setenv("CREATED", beforeDate)
+
+	if err := controller.Execute(); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	w.Write([]byte("success"))
+	log.Printf("finished")
+}
